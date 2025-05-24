c6c03524fc89f3cb75ddf73c3a600c95b1602255
aic <aic@example.com>
2025-05-24 18:45:23 +0800

    fix: Restore mock startup logic to start only in the development environment

--- src/main.ts
+++ src/main.ts
@@ -13,24 +13,17 @@ app.use(router)
 
 const mountMock = async () => {
   // if (import.meta.env.VITE_MOCK_ENABLED === 'true') {
-  // if (import.meta.env.DEV) {
-  try {
-    const { initMock } = await import('@/mocks/browser')
-    const mock = await initMock()
-    await mock.start()
-    console.log('Mock server started')
-  } catch (error) {
-    console.error('Mock server failed:', error)
+  if (import.meta.env.DEV) {
+    try {
+      const { mock } = await import('@/mocks/browser')
+      await mock.start()
+      console.log('Mock server started')
+    } catch (error) {
+      console.error('Mock server failed:', error)
+    }
   }
-  // }
 }
 
-// 该方式生产环境下有问题
-// await mountMock()
-// app.mount('#app')
+await mountMock()
 
-// 没有后台，因此生产环境下启用mock数据
-;(async () => {
-  await mountMock() // 确保 mountMock 是异步的
-  app.mount('#app')
-})()
+app.mount('#app')
