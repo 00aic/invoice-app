3ae047f3b8cc6e4b71177a1efb3d2a79a44d05fa
aic <aic@example.com>
2025-05-25 11:40:57 +0800

    Updates

---
+++ assets/api-DGOhZ8iP.js
@@ -0,0 +1,1 @@
+const o=e=>e.endsWith("/")?e.slice(0,-1):e,n=()=>(console.log("import.meta.env.BASE_URL","/invoice-app/"),`${o("/invoice-app/")}`),a={baseURL:"./",mockBaseURL:n()};export{a};
--- assets/api-tTvUdEV6.js
+++
@@ -1,1 +0,0 @@
-const a=e=>e.endsWith("/")?e.slice(0,-1):e,i={baseURL:"./",mockBaseURL:`${a("/invoice-app/")}`};export{i as a};
